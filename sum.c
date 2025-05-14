12174041d82f6c5dba8ae17219818588f340c2f2
Sagar1643 <sagar.r@s.amity.edu>
2025-05-14 14:40:56 +0530

    sum of two integer

---
+++ sum.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+    int num1, num2, sum;
+
+    // Input two numbers from user
+    printf("Enter first number: ");
+    scanf("%d", &num1);
+
+    printf("Enter second number: ");
+    scanf("%d", &num2);
+
+
+    return 0;
+}
+
